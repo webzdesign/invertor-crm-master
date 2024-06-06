01c45356026da7e2e66104a725b9f2df88653a9b
Jainil Ranpara <jainilr.ap@gmail.com>
2024-06-06 19:49:14 +0530

    timezone change

--- config/app.php
+++ config/app.php
@@ -65,7 +65,7 @@ return [
     |
     */
 
-    'timezone' => env('APP_TIMEZONE', 'Asia\Kolkata'),
+    'timezone' => env('APP_TIMEZONE', 'UTC'),
 
     /*
     |--------------------------------------------------------------------------
